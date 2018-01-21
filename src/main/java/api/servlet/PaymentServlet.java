18fb6717134feed522da0eb7634487ac39ef0f0b
PrITe <j.pooban@icloud.com>
2018-01-21 16:38:12 +0700

    payment cal

--- src/main/java/api/servlet/PaymentServlet.java
+++ src/main/java/api/servlet/PaymentServlet.java
@@ -1,6 +1,13 @@
 package api.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,16 +17,48 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
+import api.entity.Product;
+
 @WebServlet("/payment")
 public class PaymentServlet extends HttpServlet {
+	
+	public static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+	public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/wallet";
+
+	static final String USER = "root";
+	static final String PASS = "123";
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
         Map map = mapper.readValue(req.getInputStream(), Map.class);
 
-        System.out.println("Buy " + map.getProductName() + "by " + map.getName());
+        System.out.println("Buy " + map.getProductName() + " by " + map.getName());
 
+        try {
+			// Register JDBC driver
+			Class.forName(MYSQL_DRIVER);
+
+			// Open a connection
+			Connection conn = DriverManager.getConnection(MYSQL_URL, USER, PASS);
+
+			// Execute SQL query
+			Statement stmt = conn.createStatement();
+			String sql;
+			sql = "INSERT INTO wallet.receipt (orderId, description, orderDate, totalPrice, name) VALUES(1, " + map.getProductName() + ",'" + new Date() + "'," + map.getPrice() + "," + map.getName() + ")";
+			
+			PreparedStatement dbStatement = conn.prepareStatement(sql);
+			dbStatement.executeUpdate();
+			stmt.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
         mapper.writeValue(res.getOutputStream(), map);
 	}
 	
